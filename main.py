e7a5d2047176cd49d8182b3ab7180d5f8253934c
Tom <sq5rix@gmail.com>
2016-04-01 02:21:06 +0200

    threadin resolved - no () binding to Flower object - error EventDispatcher should be called

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from threading import Thread
 import serial
 
 
-class Flower(GridLayout):
+class Flower:
+
+    obj_cur_mst = ObjectProperty(None)
+    obj_cur_temp = ObjectProperty(None)
+    obj_adj_mst = ObjectProperty(None)
+    obj_avg_mst = ObjectProperty(None)
 
     def __init__(self, name, port):
         self.name = name
@@ -13,7 +18,6 @@ class Flower(GridLayout):
         self.val = ''
         self.command = ''
         self.sp = serial.Serial(self.port, baudrate=38400)
-        super(Flower, self).__init__(cols=2)
 
     def listener(self):
         c = ""
@@ -30,25 +34,18 @@ class Flower(GridLayout):
                 if num.__sizeof__() > 0:
                     self.val = num
                     if self.command == 'A':
-                        self.ids.avg_mst.text = self.val
+                        self.obj_avg_mst.text = self.val
                     elif self.command == 'M':
-                        self.ids.cur_mst.text = self.val
+                        self.obj_cur_mst.text = self.val
                     elif self.command == 'T':
-                        self.ids.cur_temp.text = self.val
+                        self.obj_cur_temp.text = self.val
                     elif self.command == 'C':
-                        self.ids.adj_mst.text = self.val
+                        self.obj_adj_mst.text = self.val
                     else:
                         pass
             else:
                 c = str(self.sp.read(), encoding='UTF-8')
                 print(c)
-
-    def run(self):
-        try:
-            t = Thread(target=self.listener())
-            t.start()
-        except:
-            print("Error: unable to start thread")
 
 
 class FlowerList:
@@ -68,11 +65,11 @@ class RootWidget(GridLayout):
 
 class MagicStalkApp(App):
 
-    #f = Flower('drosera', '/dev/ttyUSB1')
-
     def build(self):
+        f = Flower('drosera', '/dev/ttyUSB1')
+        t = Thread(target=f.listener)
+        t.start()
         return RootWidget(cols=2)
-
 
 if __name__ == '__main__':
     MagicStalkApp().run()
