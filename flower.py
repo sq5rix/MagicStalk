f5ac1d92969e6a73d73f23da726bb306da25514b
Tom <sq5rix@gmail.com>
2016-04-18 19:46:00 +0200

    first graphcd MagicStalk/

--- flower.py
+++ flower.py
@@ -1,6 +1,7 @@
 from magicerror import MagicError
 from json import dumps, loads
 import os
+import csv
 
 from kivy.properties import ObjectProperty, ListProperty, StringProperty, BooleanProperty
 from kivy.uix.screenmanager import Screen
@@ -36,14 +37,14 @@ class FlowerScreen(Screen):
 class GraphWindow(Widget):
     def __init__(self, **kwargs):
         super(GraphWindow, self).__init__(**kwargs)
-
-        #self.name = kwargs['name']
+        self.last_hour = '00'
+        self.name = 'orchid'
 
         with self.canvas:
             Color(0.08, 0.4, 0.08, .3, mode='rgba')
             self.rect = Rectangle(pos=self.pos, size=self.size)
             Color(1, 1, 1, 1)
-            #Line(points=get_moisture_from_file(name))
+            Line(points=self.get_moisture_from_file())
 
         self.bind(pos=self.update_rect)
         self.bind(size=self.update_rect)
@@ -51,6 +52,29 @@ class GraphWindow(Widget):
     def update_rect(self, *args):
         self.rect.pos = self.pos
         self.rect.size = self.size
+
+    @staticmethod
+    def in_hour(time):
+        return time.partition(':')[0]
+
+    def get_moisture_from_file(self, ):
+        # date, time, temp, moist, corr_moist
+        mp = []
+        with open('log/{}.csv'.format(self.name), 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                mp += ([row[i].strip() for i in range(4)])
+        elem = []
+        e = index = 0
+        while e < len(mp)-4:
+            x = [mp[e+i] for i in range(4)]
+            hour = self.in_hour(x[1])
+            if (hour > self.last_hour) or (hour == '00'):
+                elem += [self.pos[0]+index, self.pos[1]+int(self.size[1]*int(x[3])/100)-300]
+                self.last_hour = hour
+                index += 1
+            e += 4
+        return elem
 
 
 class Flower(FlowerScreen):
@@ -77,7 +101,7 @@ class Flower(FlowerScreen):
         self.bind(delete_flower=self.delete_this_flower)
         self.add_button_to_main()
 
-        self.graph = GraphWindow()
+        self.graph_window = GraphWindow()
 
         self.communicator = AvrParser(self.name)
         self.communicator.bind(result=self.communicator.listener)  # result from serial is ListProperty[command, val]
--- magicfiles.py
+++ magicfiles.py
@@ -1,6 +1,5 @@
 from magicerror import MagicError
 import os
-import csv
 
 
 class MagicFileWriter:
@@ -30,36 +29,4 @@ class MagicFileWriter:
         for i in args:
             self.f.write(i)
         self.f.flush()
-
-
-class MoistureGraph():
-
-    def __init__(self, name):
-        self.name = name
-        self.last_hour = '00'
-
-    @staticmethod
-    def in_hour(time):
-        return time.partition(':')[0]
-
-    def get_moisture_from_file(self):
-        # date, time, temp, moist, corr_moist
-        mp = []
-        with open('log/{}.csv'.format(self.name), 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                mp += ([row[i].strip() for i in range(4)])
-        elem = []
-        e = index = 0
-        while e < len(mp)-4:
-            x = [mp[e+i] for i in range(4)]
-            hour = self.in_hour(x[1])
-            if (hour > self.last_hour) or (hour == '00'):
-                elem += [index, int(x[3])]
-                self.last_hour = hour
-                index +=1
-            e += 4
-        return elem
-
-
 
